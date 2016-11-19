e32e62f49ddd5465bad2611216821a8d9f76d14c
Hamza Arab <hamzaarab@cmail.carleton.ca>
2016-11-19 01:04:47 -0500

    Delete AllTests.java

--- AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ConsumerTest.class, ProducerTest.class, DocumentTest.class})
-public class AllTests {
-
-}
